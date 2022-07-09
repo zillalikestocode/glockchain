235449e0d4f9c076db0461cbef4d4accc1a57b9d
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2022-07-09 19:36:32 +0100

    added more styles to the slides

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -14,47 +14,47 @@ const MoreInfo = ({links}) => {
         {links.map((link)=>{
           return (
               <div className={`my-9 ${link.bgBg} rounded-xl mx-auto w-[90%] ${link.color}`} key={link.id}>
-                <div className="p-3">
+                <div className="p-4">
                 <div className={`mt-3 ${link.bg} flex rounded-2xl items-center w-auto w-fit p-2`}>{link.icon} <label className={`text-lg font-medium ml-2 ${link.color}`}>{link.title}</label></div>
-                  <h4 className={`p-3 text-xl font-semibold text-${link.moreInfo.theme}-700`}>{link.moreInfo.header}</h4>
+                  <h4 className={`pt-4 text-xl font-semibold text-${link.moreInfo.theme}-700`}>{link.moreInfo.header}</h4>
                   </div>
-                  <style>{`
-                    
-                  `}</style>
-                  <Swiper
+				  {link.moreInfo.details && <Swiper
                     modules={[Navigation, Pagination]}
                     slidesPerView={1}
                     navigation={{prevEl: '.prev', nextEl: '.next'}}
                     pagination={{
+						clickable: true,
                      el: '.slider', type: 'bullets', 
                      bulletElement: 'span', 
-                     bulletClass: 'swiper-pagination-bullet', 
+                     bulletClass: `swiper-pagination-bullet ${link.theme}`, 
                      bulletActiveClass: `swiper-pagination-bullet-active` , 
-                     renderBullet: function(index, className){ 
-                       return `<span class="${className} !bg-${link.moreInfo.theme}-700"></span>`
-                       }}}
+                     }}
                     className="mySwiper slides p-3"
-                    style={{backgroundImage: `url(${link.moreInfo.bgImg})`}}>
+					  style={{backgroundImage: `url(${link.moreInfo.bgImg})`, backgroundSize: 'contain !important', backgoundRepeat: 'no-repeat'}}>
                     { link.moreInfo.details && link.moreInfo.details.map((item) => {
                       return (
                         <SwiperSlide key={item.id}>
-                          <div className=" py-10 slides"><img src={item.img} style={{objectFit: 'contain', width: '450px', height: '430px'}} alt="" /></div>
+							<div className=" py-10 slides"><img src={item.img} style={{objectFit: 'contain', width: '90vw', height: '430px'}} alt="" /></div>
                           <div>
-                            <h4 className="text-lg font-medium">{item.info}</h4>
-                            {item.extraInfo && <p>{item.extraInfo}</p>}
+                            <h4 className="text-lg font-medium p-4">{item.info}</h4>
+							  {item.extraInfo && <p className='p-4 pt-2'>{item.extraInfo}</p>}
                           </div>
                         </SwiperSlide>
                       )
                     })}
-					  <div className='flex p-3 items-center'>
-						<div className="flex">
-                      		<Button text={<BsArrowLeftShort size={30}/>} styles={`p-2  border rounded-md prev border-${link.moreInfo.theme}-700`} />
-                      		<Button text={<BsArrowRightShort size={30}/>} styles={`next p-2  border rounded-md border-${link.moreInfo.theme}-700`} />
+					  <div className='flex p-4 items-center'>
+						<div className="flex justify-between w-28">
+                      		<Button text={<BsArrowLeftShort size={30}/>} styles={`p-2  border rounded-md prev ${link.border}`} />
+                      		<Button text={<BsArrowRightShort size={30}/>} styles={`next p-2  border rounded-md ${link.border}`} />
                     	</div>
-                    	<div className=" slider ml-auto"></div>
+                    	<div className="!w-28 !flex slider !ml-auto !justify-between"></div>
 					  </div>
-                    
-                  </Swiper>
+                  </Swiper>}
+				  <div className="p-4" style={{ backgroundImage: `url(${link.moreInfo.bgImg})`, objectFit: 'contain',  }}>
+					 {link.moreInfo.info && <div className="p-4" ><p className="text-md">{link.moreInfo.info}</p></div>}
+					<Button text={link.moreInfo.btnText} styles={` p-4 ${link.theme} rounded-lg text-white`}/>
+				  </div>
+				  
               </div>
           )
         })}
